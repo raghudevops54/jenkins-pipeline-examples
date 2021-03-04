8635105ce2a68599e83461754ca3579a2eeaade0
KalluruS <>
2021-03-04 07:20:38 +0530

    Update jenkins-pipeline-examples/

--- jenkins-slave-setup/setup.sh
+++ jenkins-slave-setup/setup.sh
@@ -56,7 +56,7 @@ TOKEN=$(curl -s -u ${USERNAME}:${PASSWORD} ${URL}/computer/${AGENTNAME}/slave-ag
 
 curl -f -s -O ${URL}/jnlpJars/agent.jar
 
-sed -i -e "s/URL/${URL}/" -e "s/AGENTNAME/${AGENTNAME}/" -e "s/TOKEN/${TOKEN}/" slave.service
+sed -i -e "s|URL|${URL}|" -e "s/AGENTNAME/${AGENTNAME}/" -e "s/TOKEN/${TOKEN}/" slave.service
 cat slave.service
 
 sudo cp slave.service /etc/systemd/system/jenkins-slave.service
